d719edff6373615ad88b7c6ba664fecc59491057
Justen Doherty <justen@wedo.co.uk>
2014-03-09 21:01:35 +0000

    Dropped social media links in

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -1,29 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-Route::group(array('before' => 'getSubdomain'), function()
-{
-
-//        if(!empty($_SESSION['redirected']) || $_SESSION['redirected'] != true) {
-//            $host = Request::getHost();
-//            $parts = explode('.', $host);
-//            $subdomain = $parts[0];
-//            $_SESSION['redirected'] = true;
-//            return Redirect::route('presentation');
-//        } else {
-//            die('#');
-//        }
-});
-
 // main website
 Route::get('/', function(){    
     return View::make('index');
