de014dffbd0c47cf89d836502a28aedca832b938
Justen Doherty <justen@wedo.co.uk>
2014-08-21 23:11:40 +0100

    Added FF and Cricket

---
+++ laravel/app/views/frenzy-fox.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Frenzy Fox - Appbox Media</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/css/iphoneapp.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="/js/jquery.js"></script>
+    <script src="/js/bootstrap.js"></script>
+    <link
+        href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Graduate|Cutive|Wellfleet|Bevan'
+        rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+        $(function () {
+            $('.carousel').carousel({
+                interval: 2000
+            })
+        });
+    </script>
+</head>
+
+<body class="features-facts back-black">
+
+<div class="container iphone-container">
+    <div class="iphone-landscape iphone-4s iphone-white">
+        <h1><img src="/img/FrenzyFox_Logo.png"/></h1>
+        <div class="iphone-block">
+            <div class="iphone">
+                <div id="myCarousel" class="iphone-screen carousel slide">
+                    <div class="iphone-glare"></div>
+                    <iframe width="380" height="280" src="//www.youtube.com/embed/V81xozj9qR8?autoplay=1" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="iphone-shadow"></div>
+        </div>
+        <div class="description">
+            <div class="features">
+                <div class="feature">
+                    <div class="feature-inner">
+                        <h2>In the forest only the fastest survive!</h2>
+                        <p>AppBox Media presents Frenzy Fox, the free to play infinitely challenging forest based running game. Challenge your reflexes as you collect fruit for bonus multipliers, clear obstacles and get that high score, share with friends and brag about who’s the king of the jungle.</p>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="span4"><img style="padding: 10px 0;float: right" class="img-responsive" src="/img/FrenzyFox.png"/></div>
+    <div class="span4"><img style="padding: 10px 0" class="img-responsive" src="/img/download-googleplay.png"/></div>
+    <div class="span4"><img style="padding: 10px 0;height:120px;" class="img-responsive" src="/img/download-appstore.png"/></div>
+</div>
+
+<div class="gradient-line gradient-line-before"></div>
+<div class="container">
+    <div class="response-line">
+        <footer>
+            <ul class="Social">
+                <li>
+                    <a data-original-title="Facebook" target="_blank" href="https://www.facebook.com/AppBoxMedia" rel="tooltip" class="Facebook">Facebook</a>
+                </li>
+                <li>
+                    <a data-original-title="Twitter" target="_blank" href="https://twitter.com/AppboxMedia" rel="tooltip" class="Twitter">Twitter</a>
+                </li>
+                <li>
+                    <a data-original-title="Pinterest" href="#" rel="tooltip" class="Pinterest">Pinterest</a>
+                </li>
+                <li>
+                    <a class="Googleplus" rel="tooltip" href="#" data-original-title="Googleplus">Googleplus</a>
+                </li>
+                <li>
+                    <a class="Instagram" rel="tooltip" href="#" data-original-title="Instagram">Instagram</a>
+                </li>
+                <li>
+                    <div style="margin:0 auto;width:240px">
+                        <a href="#Home" id="logo_bottom_flip" class="brand"></a>
+                    </div>
+                </li>
+
+            </ul>
+
+        </footer>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+
+    </div>
+</footer>
+</body>
+</html>
