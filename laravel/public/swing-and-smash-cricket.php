87029aa62b00184f511667b84b1aaa9fee2326a3
Justen Doherty <justen@wedo.co.uk>
2014-10-01 21:50:09 +0100

    Added things for .com

---
+++ laravel/public/swing-and-smash-cricket.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Swing and Smash Cricket - Appbox Media</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/css/iphoneapp.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="/js/jquery.js"></script>
+    <script src="/js/bootstrap.js"></script>
+    <link
+        href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Graduate|Cutive|Wellfleet|Bevan'
+        rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+        $(function () {
+            $('.carousel').carousel({
+                interval: 2000
+            })
+        });
+    </script>
+</head>
+
+<body class="features-facts back-black">
+
+<div class="container iphone-container">
+    <div class="iphone-landscape iphone-4s iphone-white">
+        <h1><img style="width: 60%" src="/img/swingandsmashcover.png"/></h1>
+        <div class="iphone-block">
+            <div class="iphone">
+                <div id="myCarousel" class="iphone-screen carousel slide">
+                    <div class="iphone-glare"></div>
+                    <iframe width="380" height="280" src="//www.youtube.com/embed/t_JzsS_3mvg?autoplay=1" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="iphone-shadow"></div>
+        </div>
+        <div class="description">
+            <div class="features">
+                <div class="feature">
+                    <div class="feature-inner">
+                        <h2>Who will be crowned the best and biggest batter in town?</h2>
+                        <p>AppBox Media presents their latest title: Swing and Smash Cricket! Inspired by cricket and baseball all stars, AppBox Media challenge you to a smash all of the targets from the unpredictable bowler and achieve a huge score.</p>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="span4"><img  class="img-responsive fox" src="/img/swingandsmashcricket.png"/></div>
+    <div class="span4"><a href="https://play.google.com/store/apps/details?id=com.AppBoxMedia.Cricket&hl=en_GB" target="_blank"><img style="padding: 10px 0;height:90px;" class="img-responsive" src="/img/download-googleplay.png"/></a></div>
+    <div class="span4"><a href="#" target="_blank"><img style="padding: 10px 0;height:95px;" class="img-responsive" src="/img/download-appstore.png"/></a></div>
+</div>
+
+<div class="gradient-line gradient-line-before"></div>
+<div class="container">
+    <div class="response-line">
+        <footer>
+            <ul class="Social">
+                <li>
+                    <a data-original-title="Facebook" target="_blank" href="https://www.facebook.com/AppBoxMedia" rel="tooltip" class="Facebook">Facebook</a>
+                </li>
+                <li>
+                    <a data-original-title="Twitter" target="_blank" href="https://twitter.com/AppboxMedia" rel="tooltip" class="Twitter">Twitter</a>
+                </li>
+                <li>
+                    <a data-original-title="Pinterest" href="#" rel="tooltip" class="Pinterest">Pinterest</a>
+                </li>
+                <li>
+                    <a class="Googleplus" rel="tooltip" href="#" data-original-title="Googleplus">Googleplus</a>
+                </li>
+                <li>
+                    <a class="Instagram" rel="tooltip" href="#" data-original-title="Instagram">Instagram</a>
+                </li>
+            </ul>
+            <div style="clear: both" id="link">
+
+                <a href="#Home" id="logo_bottom_flip" class="brand"></a>
+            </div>
+
+        </footer>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+
+    </div>
+</footer>
+</body>
+</html>
